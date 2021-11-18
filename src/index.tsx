634e3d50cedbf652bd2457529c9a9117419fd0e0
oguzturker8 <oguzturker8@gmail.com>
2021-11-18 13:50:45 +0300

    feat(header): add EdgeInsets type

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
+export type { ParamListBase, Route } from '@react-navigation/routers';
+export type { StackNavigationProp } from '@react-navigation/stack';
+export type { Scene } from '@react-navigation/stack/lib/typescript/src/types';
 export { default as Header } from './components/Header';
 export type {
   Callback,
+  EdgeInsets,
   HeaderStyle,
   HeaderType,
   LeftType,
